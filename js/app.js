5c42c4a7794a009f04e7847bb8b7c29b612e5ee0
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-09-07 00:10:38 -0600

    added detail.html added some style to it what remains is it laptop and tab view

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+
+const getUniqueChr = (string)=>{
+    let unqie =''
+    for(  char of string){
+        if (!unqie.includes(char)){
+        unqie+=char;
+            
+        }
+
+    }
+    return unqie;
+}
+
+// console.log(getUniqueChr("hello")=='helo')
+
+
+const mostOccured =(list)=>{
+    let higestObj = Object();
+    let count=0;
+    while(count <= list.length-1){
+        if(higestObj[`${list[count]}`] == undefined){
+
+        higestObj[`${list[count]}`] = 0
+        }
+
+        for(i in list){
+            if (i==list[count]){
+                // console.log(i)
+                higestObj[`${list[count]}`]+=1
+            }
+        }
+        count+=1
+    }
+    console.log(higestObj)
+}
+
+mostOccured([1,2,3,3,3])
